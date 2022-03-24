ccaad781a02cf60d49c5259c679a7f1c3cce373f
Corundrum <Simon.Sereda@georgebrown.ca>
2022-03-23 20:16:09 -0400

    Finished Platform Player Class

--- LE4_SeredaSimon/GAME1017_EmptyEngine/PlatformPlayer.cpp
+++ LE4_SeredaSimon/GAME1017_EmptyEngine/PlatformPlayer.cpp
@@ -1,60 +1,131 @@
 #include "PlatformPlayer.h"
+#include "EventManager.h"
+#include "TextureManager.h"
+#include <cmath>
 
-PlatformPlayer::PlatformPlayer(SDL_Rect s, SDL_FRect d)
+PlatformPlayer::PlatformPlayer(SDL_Rect s, SDL_FRect d) : AnimatedSpriteObject(s, d), m_state(STATE_JUMPING), m_isGrounded(false), m_isFacingLeft(false),
+m_maxVelX(9.0), m_maxVelY(JUMPFORCE), m_grav(GRAVITY), m_drag(0.85)
 {
+	m_accelX = m_accelY = m_velX = m_velY = 0.0;
 }
 
-void PlatformPlayer::Update()
+void PlatformPlayer::Update() 
 {
+	switch (m_state)
+	{
+	case STATE_IDLING:
+		//Transition to Run
+		if (EVMA::KeyPressed(SDL_SCANCODE_A) || EVMA::KeyPressed(SDL_SCANCODE_D))
+		{
+			m_state = STATE_RUNNING;
+			//SetAnimation;
+		}
+		//Transition to Jump
+		else if (EVMA::KeyPressed(SDL_SCANCODE_SPACE) && m_isGrounded)
+		{
+			m_accelY = -JUMPFORCE;
+			m_isGrounded = false;
+			m_state = STATE_JUMPING;
+			//SetAnimation-----
+		}
+		break;
+	case STATE_RUNNING:
+		//Move on ground
+		if (EVMA::KeyHeld(SDL_SCANCODE_A))
+		{
+			m_accelX = -1.5;
+			if (!m_isFacingLeft)
+				m_isFacingLeft = true;
+		}
+		else if (EVMA::KeyHeld(SDL_SCANCODE_D))
+		{
+			m_accelX = 1.5;
+			if (m_isFacingLeft)
+				m_isFacingLeft = false;
+		}
+		//Transition to Idle
+		if (!EVMA::KeyHeld(SDL_SCANCODE_A) && !EVMA::KeyHeld(SDL_SCANCODE_D))
+		{
+			m_state = STATE_IDLING;
+			//SetAnimation-----
+		}
+		//Transition to Jump
+		else if (EVMA::KeyPressed(SDL_SCANCODE_SPACE) && m_isGrounded)
+		{
+			m_accelY = -JUMPFORCE;
+			m_isGrounded = false;
+			m_state = STATE_JUMPING;
+			//SetAnimation-----
+		}
+		break;
+	case STATE_JUMPING:
+		//move in mid air
+		if (EVMA::KeyHeld(SDL_SCANCODE_A) && m_dst.x > 0)
+		{
+			m_accelX = -1.5;
+			if(!m_isFacingLeft)
+				m_isFacingLeft = true;
+		}
+		else if (EVMA::KeyHeld(SDL_SCANCODE_D) && m_dst.x > WIDTH - m_dst.w)
+		{
+			m_accelX = 1.5;
+			if (m_isFacingLeft)
+				m_isFacingLeft = false;
+		}
+		//transition to run
+		if (m_isGrounded)
+		{
+			m_state = STATE_RUNNING;
+			// SetAnimation(?,?,?,?)-----
+		}
+
+		break;
+	}
+
+	//PLAYER MOVEMENT
+	m_velX += m_accelX;
+	m_velX *= (m_isGrounded ? m_drag : 1.0);
+	m_velX = std::min(std::max(m_velX, -m_maxVelX), m_maxVelX);
+	m_dst.x += (float)m_velX;
+	//Wrap the Player on screen
+	
+	m_velY += m_accelY + m_grav;
+	m_velY = std::min(std::max(m_velY, -m_maxVelY), m_maxVelY);
+	m_dst.y += (float)m_velY;
+
+	m_accelX = m_accelY = 0;
+	// Animate();
 }
 
-void PlatformPlayer::Render()
-{
+void PlatformPlayer::Render() 
+{ 
+	//to animate the sprite, use SDL_RenderCopyExF()... will have to access the texturemanager and pass in a key such as "player".
+	SDL_SetRenderDrawColor(Engine::Instance().GetRenderer(), 255, 0, 0, 255);
+	SDL_RenderFillRectF(Engine::Instance().GetRenderer(), &m_dst);
 }
 
-void PlatformPlayer::Stop()
+void PlatformPlayer::Stop() 
 {
+	StopX();
+	StopY();
 }
 
-void PlatformPlayer::StopX()
-{
-}
+void PlatformPlayer::StopX() { m_velX = 0.0; }
 
-void PlatformPlayer::StopY()
-{
-}
+void PlatformPlayer::StopY() { m_velY = 0.0; }
 
-void PlatformPlayer::SetAccelX(double a)
-{
-}
+void PlatformPlayer::SetAccelX(double a) { m_accelX = a; }
 
-void PlatformPlayer::SetAccelY(double a)
-{
-}
+void PlatformPlayer::SetAccelY(double a) { m_accelY = a; }
 
-void PlatformPlayer::SetX(float x)
-{
-}
+void PlatformPlayer::SetX(float x) { m_dst.x = x; }
 
-void PlatformPlayer::SetY(float y)
-{
-}
+void PlatformPlayer::SetY(float y) { m_dst.y = y; }
 
-void PlatformPlayer::SetGrounded(bool g)
-{
-}
+void PlatformPlayer::SetGrounded(bool g) { m_isGrounded = g; }
 
-bool PlatformPlayer::IsGrounded()
-{
-	return false;
-}
+bool PlatformPlayer::IsGrounded() { return m_isGrounded; }
 
-double PlatformPlayer::GetVelX()
-{
-	return 0.0;
-}
+double PlatformPlayer::GetVelX() { return m_velX; }
 
-double PlatformPlayer::GetVelY()
-{
-	return 0.0;
-}
+double PlatformPlayer::GetVelY() { return m_velY; }
